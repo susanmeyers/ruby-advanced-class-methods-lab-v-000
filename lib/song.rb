a898361fc8d71a0ff1db628d5470a42ed74060e8
Susan Meyers <smmeyers24@gmail.com>
2018-09-17 18:08:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,89 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+
 
   def self.all
     @@all
   end
 
+
+
   def save
     self.class.all << self
   end
+
+
+#a class method that .create that instantiates and saves a song and returns the new song created
+
+  def self.create
+    song = self.new
+     @@all << song
+      song
+  end
+
+
+  #Class method new_by_name to instantiate a song with a new property
+
+  def self.new_by_name(name)
+    song = self.create
+     song.name = (name)
+      song
+
+  end
+
+
+  def self.create_by_name(name)
+     song = self.create
+       song.name = (name)
+         song
+  end
+
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+
+  def self.find_or_create_by_name(name)
+      if self.find_by_name(name) != nil
+        self.find_by_name(name)
+
+      else
+      create_by_name(name)
+      end
+    end
+
+
+
+  def self.alphabetical
+       @@all.sort_by do |song|
+        song.name
+       end
+  end
+
+
+  def self.new_from_filename(filename)
+    array = filename.split(" - ")
+    song_name = array[1].gsub(".mp3", "")
+    name_of_artist = array[0]
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = name_of_artist
+
+    song
+  end
+
+  
+
+  def self.create_from_filename(filename)
+      song_name_two = self.new_from_filename(filename)
+       song.name = song_name_two
+         song
+
+
 
 end
